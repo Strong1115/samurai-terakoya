a288b3d3ab0601f5f2b1b52281714403a56dd516
Strong1115 <120384573+Strong1115@users.noreply.github.com>
2022-12-19 23:09:36 +0900

    Delete Main4_5.java

--- section_04/Main4_5.java
+++
@@ -1,9 +0,0 @@
-public class Main4_5 {
-	public static void main(String[] args) { 
-	    int [] scores;
-	    scores = new int [5];
-	    scores[1] = 30;
-	    scores[0] = 50;
-	    System.out.println(scores[2]);
-	}
-}
