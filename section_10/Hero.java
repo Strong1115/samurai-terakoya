da233ef833ce315e3d3060a9ae44a4f68710f2f1
Strong1115 <kowase1115@gmail.com>
2022-12-22 00:03:56 +0900

    コミット12/21

---
+++ section_10/Hero.java
@@ -0,0 +1,27 @@
+public class Hero {
+    String name;
+    int hp = 100;
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    // 戦う
+    public void attack(Matango10_q10_3 m) {
+        System.out.println(this.name + "の攻撃!");
+        m.hp -= 5;
+        System.out.println("お化けキノコ" + m.suffix + "に5ポイントのダメージをあたえた!");
+    }
+
+    public void attack(PoisonMatango pm) {
+        System.out.println(this.name + "の攻撃!");
+        pm.hp -= 5;
+        System.out.println("お化け毒キノコ" + pm.suffix + "に5ポイントのダメージをあたえた!");
+    }
+
+    // 逃げる
+    public void run() {
+        System.out.println(this.name + "は逃げ出した!");
+    }
+
+}
